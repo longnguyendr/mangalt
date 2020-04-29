3c8f0bc23398557bce280e94e2a280432cada61a
Long <long.nguyendr@gmail.com>
2020-04-29 16:41:03 +0300

    update backend

---
+++ backend/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+requires = [
+    'pyramid',
+    'waitress',
+]
+dev_requires = [
+    'pyramid_debugtoolbar',
+    'pytest',
+    'webtest',
+]
+setup(
+    name = 'mangalt',
+    install_requires=requires,
+    extras_require={
+        'dev': dev_requires,
+    },
+    entry_points={
+        'paste.app_factory': [
+            'main = mangalt:main'
+        ],
+    },
+)
